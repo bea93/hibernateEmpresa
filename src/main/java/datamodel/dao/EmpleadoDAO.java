6b876910f4f88f1867136874e3774c42a8981630
bea93 <bmerinomacia@gmail.com>
2021-10-27 13:29:02 +0200

    Proximamente

--- src/main/java/datamodel/dao/EmpleadoDAO.java
+++ src/main/java/datamodel/dao/EmpleadoDAO.java
@@ -1,6 +1,14 @@
 package datamodel.dao;
 
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import datamodel.entities.annotations.Empleado;
 
@@ -22,6 +30,18 @@ public class EmpleadoDAO {
 		s.delete(empleado);
 	}
 
-	
+	//Consulta con criteria
+		public static Empleado consultarEmpleado(Session s, int cod_departamento) {
+			CriteriaBuilder builder = s.getCriteriaBuilder();
+			CriteriaQuery<Empleado> query = builder.createQuery(Empleado.class);
+			Root<Empleado> root = query.from(Empleado.class);
+	    	query.select(root).where(builder.equal(root.get("codDepartamento"), cod_departamento));
+	    	Query<Empleado> q = s.createQuery(query);
+	    	List<Empleado> result = (List<Empleado>) q.list();
+			for(Empleado e : result) {
+				System.out.println(e.toString());
+			}
+	    	return null;
+		}
 	
 }
